c4d2ac2479bc58a0d69d4f0fd1e50f434b252b9b
benjaminkent <benjamin.jehl7@gmail.com>
2024-08-16 15:44:52 -0400

    add blank component

---
+++ src/components/SlotUpload.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>this will be the upload ocmpnent</div>
+</template>
+
+<script setup lang="ts"></script>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="flex flex-col items-center gap-8 bg-gray-900 h-screen pt-28">
-    upload coponent going here
+    <SlotUpload />
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SlotUpload from '@/components/SlotUpload.vue';
+</script>
